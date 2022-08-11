67cb6e365a0f7a95552af1126d80f5cfaa995820
Christian Vargas <cristhiandavid.vargas@vmlyr.com>
2022-08-10 23:14:37 -0500

    solución 3

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 // alert('Soy un alert');
 
 // 1. entrada datos usuario para cambiar tema
-const seleccionTema = confirm('¿Deseas el tema oscuro en la landing?....');
+// const seleccionTema = confirm('¿Deseas el tema oscuro en la landing?....');
 // console.log(seleccionTema);
 
 // 2. Evaluar la selección del usuario para escoger el tema
 // console.log(document.querySelector('body').classList);
 // if(seleccionTema) {
-//     document.querySelector('body').classList.add('dark');
-//     // console.log(document.querySelector('body'));
+    //     document.querySelector('body').classList.add('dark');
+    //     // console.log(document.querySelector('body'));
 // }
 
 // solucion 2
-let main = seleccionTema => {
-    return seleccionTema ? document.querySelector('body').classList.add('dark') : '';
-}
+// let main = seleccionTema => {
+//     return seleccionTema ? document.querySelector('body').classList.add('dark') : '';
+// }
 
-main(seleccionTema);
-
+// main(seleccionTema);
+// solucion 3
+const seleccionTema2 = confirm('¿Deseas el tema oscuro en la landing?....') ? document.querySelector('body').classList.add('dark') : '';
 // 3. ejecutar la seleccion
